e1869dad5ff78e2204a54bdc194ae79a98c4c052
Aren-R <arenrepko7@gmail.com>
2023-05-30 22:40:39 +0200

    Finalised the signup page, added winery case

--- signup.php
+++ signup.php
@@ -6,12 +6,7 @@
         $email = trim($_POST['email']);
         $password = trim($_POST['password']);
 
-        if(isset($_POST['name']) && isset($_POST['country']) && isset($_POST['region'])){
-            $name = trim($_POST['name']);
-            $country = trim($_POST['country']);
-            $region = trim($_POST['region']);
-            $isWinery = true;
-        }
+
         // Check if the email already exists in the database
         $stmt = $conn->prepare('SELECT COUNT(*) FROM user WHERE UserID = ?');
         $stmt->bind_param('s', $email);
@@ -25,10 +20,26 @@
             exit;
         }
 
-        if (!preg_match('/^[a-zA-Z\s]+$/', $name)) {
-            echo "Invalid name";
-            exit;
+        if(isset($_POST['name']) && isset($_POST['country']) && isset($_POST['region'])){
+            $name = trim($_POST['name']);
+            $country = trim($_POST['country']);
+            $region = trim($_POST['region']);
+            
+            if (!preg_match('/^[a-zA-Z\s]+$/', $name)) {
+                echo "Invalid name";
+                exit;
+            }
+            if (!preg_match('/^[a-zA-Z\s]+$/', $country)) {
+                echo "Invalid country";
+                exit;
+            }
+            if (!preg_match('/^[a-zA-Z\s]+$/', $region)) {
+                echo "Invalid region";
+                exit;
+            }
+            $isWinery = true;
         }
+
         //the industry standard regex for email as found on http://www.ex-parrot.com/~pdw/Mail-RFC822-Address.html
         if (!preg_match('/^(?:[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]/', $email)) {
             echo "Invalid email";
@@ -49,17 +60,27 @@
         $salt = bin2hex(random_bytes(32));
         $hashedPassword = password_hash($password . $salt, PASSWORD_ARGON2ID, $options2);
         // Create the user in the database
-        if(!$isWinery){
-        $stmt = $conn->prepare('INSERT INTO COS221_User (UserID, password, salt, WineryID) VALUES (?, ?, ?, ?)');
-        $stmt->bind_param('sssi', $email, $hashedPassword, $salt, NULL);
-        $stmt->execute();
-        $stmt->close();
-        echo "<script>";
-        echo "localStorage.setItem(\"WineryID\", NULL);";
-        echo " window.location.href = 'index.php';";
-        echo "</script>";
-        }else {
-            # code...
+        if (!$isWinery) {
+            $stmt = $conn->prepare('INSERT INTO COS221_User (UserID, password, salt, WineryID) VALUES (?, ?, ?, ?)');
+            $stmt->bind_param('sssi', $email, $hashedPassword, $salt, NULL);
+            $stmt->execute();
+            $stmt->close();
+        }  else {
+            $stmt = $conn->prepare('INSERT INTO Winery (Country, Region, Name,) VALUES (?, ?, ?)');
+            $stmt->bind_param('sss', $country, $region, $name);
+            $stmt->execute();
+            $stmt->close();
+            $stmt = $conn->prepare('SELECT MAX(WineryID) FROM COS221_Winery WHERE Name = ?');
+            $stmt->bind_param('s', $name);
+            $stmt->execute();
+            $wineryID;
+            $stmt->bind_result($wineryID);
+            $stmt->fetch();
+            $stmt->close();
+            $stmt = $conn->prepare('INSERT INTO COS221_User (UserID, password, salt, WineryID) VALUES (?, ?, ?, ?)');
+            $stmt->bind_param('sssi', $email, $hashedPassword, $salt, $wineryID);
+            $stmt->execute();
+            $stmt->close();
         }
         echo "<script>";
         echo " window.location.href = 'login.php';";
