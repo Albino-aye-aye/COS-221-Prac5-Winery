740ab42e5d8fbfca9447f9476bbc6eaa69e157d4
Aren-R <arenrepko7@gmail.com>
2023-05-30 22:57:04 +0200

    fixed conn

--- signup.php
+++ signup.php
@@ -1,5 +1,5 @@
 <?php
-    require 'config.php';
+    require_once 'config.php';
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Retrieve the input values from the form
         $isWinery = false;
