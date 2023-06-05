4c1ff3e19a70a346682bbf75335c803c8fe25abe
Aren-R <arenrepko7@gmail.com>
2023-06-05 14:13:25 +0200

    Added styling and made nav

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link href="style.css" rel="stylesheet">
+    <title>Website</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand navbar-dark bg-dark height">
+        <ul class="navbar-nav mx-auto justify-content-center navItemContainer" id = "navBar">
+            <li class="nav-item"><a class="nav-link" href="wines.php">Wines</a></li>
+            <li class="nav-item"><a class="nav-link" href="manage_wines.html">Manage Wines</a></li>
+            <li class="nav-item"><a class="nav-link" href="search.php">Find a Wine</a></li>
+            <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+            <li class="nav-item"><a class="nav-link" href="signup.php">Sign Up</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
