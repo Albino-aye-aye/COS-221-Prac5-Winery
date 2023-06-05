f395179434af8c583df252bda4ae039026890f8d
Alec Watson <111232411+Albino-aye-aye@users.noreply.github.com>
2023-06-05 20:18:06 +0200

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
- $db_host = 'wheatley.cs.up.ac.za';
-    $db_name = 'u22570285';
-    $db_user = 'u22570285';
-    $db_pass = 'O274QLGXCRJUQ73NVGWHPHPSO3CV3NDV';
-    // Connect to the database
-    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
-    ?>
