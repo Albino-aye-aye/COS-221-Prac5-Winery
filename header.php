4ae03d3f677a01c391dd9204332455248811b58f
Alec Watson <111232411+Albino-aye-aye@users.noreply.github.com>
2023-06-06 17:13:54 +0000

    Update header.php

--- header.php
+++ header.php
@@ -19,11 +19,11 @@ include_once('config.php');
             if (wineryID && wineryID !== 'null') {
                 $('.nav-link[href="login.php"]').replaceWith('<li class="nav-item"><a class="nav-link" href="users.php">User Management</a></li>');
                 $('.nav-link[href="signup.php"]').replaceWith('<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>');
+                $('.nav-link[href="manage_wines_html.php"]').parent().show(); // Show the manage_wines_html.php button
                 $('.nav-link[href="review.php"]').parent().hide(); // Hide the list item containing the review.php button
             } else {
                 $('.nav-link[href="login.php"]').replaceWith('<li class="nav-item"><a class="nav-link" href="users.php">User Management</a></li>');
                 $('.nav-link[href="signup.php"]').replaceWith('<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>');
-                $('.nav-link[href="manage_wines_html.php"]').show(); // Show the manage_wines_html.php button
                 $('.nav-link[href="review.php"]').parent().show(); // Show the list item containing the review.php button
             }
         });
@@ -33,7 +33,7 @@ include_once('config.php');
     <nav class="navbar navbar-expand navbar-dark bg-dark height">
         <ul class="navbar-nav mx-auto justify-content-center navItemContainer" id="navBar">
             <li class="nav-item"><a class="nav-link" href="wines.php">Wines</a></li>
-            <li class="nav-item"><a class="nav-link" href="manage_wines_html.php">Manage Wines</a></li>
+            <li class="nav-item" style="display: none;"><a class="nav-link" href="manage_wines_html.php">Manage Wines</a></li>
             <li class="nav-item"><a class="nav-link" href="search.php">Find Winery</a></li>
             <li class="nav-item" style="display: none;"><a class="nav-link" href="review.php">Review Wine</a></li>
             <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
